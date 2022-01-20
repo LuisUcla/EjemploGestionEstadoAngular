7f77edbe5f46f38a00493536e49f5c517afd58a9
LuisUcla <caluigi7@gmail.com>
2022-01-20 16:49:31 -0400

    realizacion de manejo de estado

--- parking-lot-app/src/app/app.component.ts
+++ parking-lot-app/src/app/app.component.ts
@@ -1,17 +1,30 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { StoreService } from './store/store.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [StoreService]
 })
 export class AppComponent {
   title = 'parking-lot-app';
+  plate = ''
+  vm$ = this.store.vm$
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private store: StoreService) {}
 
-  carsList() {
-    this.router.navigateByUrl("car-list");
+  onSubmit($event: Event) {
+    $event.preventDefault()
+    this.store.addCarToParkingLot(this.plate)
+  }
+
+  addPlate($event: Event) {
+    const target = $event.target as HTMLButtonElement
+
+    if (target.nodeName === 'BUTTON') {
+        this.plate = target.innerHTML
+    }
   }
 }
--- parking-lot-app/src/app/components/car-list/car-list.component.ts
+++ parking-lot-app/src/app/components/car-list/car-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Car } from 'src/app/models/car';
 
 @Component({
   selector: 'app-car-list',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarListComponent implements OnInit {
 
+  @Input() cars: Car[] = []
   constructor() { }
 
   ngOnInit(): void {
